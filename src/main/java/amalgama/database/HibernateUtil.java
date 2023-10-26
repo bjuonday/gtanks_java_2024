5a2dc94ed8e1d22443207a8b1568d797f91ae4a0
4malgama <newulla@inbox.ru>
2023-10-27 00:07:56 +0600

    just commit

---
+++ src/main/java/amalgama/database/HibernateUtil.java
@@ -0,0 +1,20 @@
+package amalgama.database;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory factory;
+
+    static {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getFactory() {
+        return factory;
+    }
+}
